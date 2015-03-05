bf008a551cc5f6e10cd2bf722c34995f9eb0d185
John van Loon <john.vanloon@mohawkcollege.ca>
2015-03-05 11:21:24 -0500

    Added documentation
    Added documentation example to functions in enc28j60.c

--- enc28J60.c
+++ enc28J60.c
@@ -5,9 +5,53 @@
  *  Author: John
  */ 
 #include <avr/io.h>
+#include "SPI.h"
 #include "enc28J60.h"
 
+/** 
+ * function ENC28J60_init
+ * \brief Initialization of the enc28J60
+ *
+ * Provides initialization functionality for the ENC28J60 chip via SPI.
+ * \param[in] RXsize The size of the RX memory area to set up in the enc28j60
+ * \param[in] TXsize The size of the TX memory area to set up in the enc28j60
+ * \param[in] Broadcast Flag to indicate if a broadcast is received. 1- receive broadcast packets
+ */
+ 
 void ENC28J60_init(uint16_t RXsize, uint16_t TXsize, uint8_t Broadcast)
 {
 	// Initialize the ENC28J60 for the following set up
+}
+
+
+/** 
+ * function ENC28J60_config_LEDs
+ * \brief Change behaviour of the enc28J60 LED outputs (on jack)
+ *
+ * Allows the behaviour of the LEDs to be configured.
+ *
+ * Flag values for LEDA and LEDB can take the values:
+ *-		ENC28J60_LED_ON
+ *-		ENC28J60_LED_OFF
+ *-		ENC28J60_LED_LINK_TXRX
+ *-		ENC28J60_LED_LINK
+ *-		ENC28J60_LED_COL_ACTIVITY
+ *-		ENC28J60_LED_TXRX_ACTIVITY
+ *-		ENC28J60_LED_BLINK_SLOW
+ *-		ENC28J60_LED_BLINK_FAST 
+ *
+ *
+ * Stretch times are specified as 
+ *- ENC28J60_TLSTRCH
+ *- ENC28J60_TMSTRCH
+ *- ENC28J60_TNSTRCH
+ *- ENC28J60_NOSTRCH
+ *
+ * \param[in] ledA Flag indicating the operation of LEDA
+ * \param[in] ledB Flag indicating the operation of LEDB
+ * \param[in] led_stretch determines led stretch time. Upper 4 bits are for LEDA, Lower are for LEDB
+ */
+void ENC28J60_config_LEDs(uint8_t ledA, uint8_t ledB, uint8_t led_stretch)
+{
+	
 }
--- enc28J60.h
+++ enc28J60.h
@@ -5,5 +5,11 @@
 /// Defines
 /// 
 
-void ENC28J60_init(uint16_t RXsize, uint16_t TXsize);
+/************************************************************************/
+/* Function declarations                                                */
+/************************************************************************/
+void ENC28J60_init(uint16_t RXsize, uint16_t TXsize, uint8_t Broadcast);
+
+void ENC28J60_config_LEDs(uint8_t ledA, uint8_t ledB, uint8_t led_stretch);
+
 #endif
