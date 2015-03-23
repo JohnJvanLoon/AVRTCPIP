3605fc8b094282de6e5f234ca481015cd7477430
Nathaniel Tammer <natetamer@gmail.com>
2015-03-23 11:26:28 -0400

    adding spi read and timer code update

--- SPI.h
+++ SPI.h
@@ -1,8 +1,12 @@
 #ifndef SPI_H
 #define SPI_H
 
+void spi_init(void);
+uint8_t SPI_Release (void);
+uint8_t spi_run_state(void);
 uint8_t spi_request_attach(void);
 uint8_t spi_TXRX_data(uint8_t len, uint8_t * data);
-int SPI_CheckComplete(void);
+uint8_t SPI_CheckComplete(void);
+uint8_t SPI_read_data(uint8_t *data, uint8_t len);
 
 #endif
