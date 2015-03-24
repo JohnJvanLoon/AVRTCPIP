db217419878603ada5cf3b2f7427d559a0e76719
MystreCoder <MystreCoder@users.noreply.github.com>
2015-03-24 13:44:38 -0400

    Replaced the state run code into the file.

--- enc28J60.c
+++ enc28J60.c
@@ -12,6 +12,49 @@ void SPI_WAIT(void);	//creates wait function function
 void WRITE_ENC28_CTRL(uint8_t ARGUMENT, uint8_t data); //creates the registry write function 
 void MAC_Init(void); 	//creates initialization function
 
+typedef enum  {Idle, Ready_To_Send, RW_Register, RW_Data, Complete} enc28j60_comm_states;
+	
+typedef struct
+{
+	enc28j60_comm_states state;
+}
+enc28j60_comm_struct;
+
+volatile enc28j60_comm_struct enc28j60_comm_data; // global variable for the enc28j60 communication data 
+
+
+int enc28j60_comm(void)
+{
+	uint8_t ret_val=0;
+	switch (enc28j60_comm_data.state)
+	{
+		case Idle:
+			
+			
+		break;
+		case Ready_To_Send:
+			
+			
+		break;
+		case RW_Register:
+			
+			
+		break;
+		case RW_Data:
+			
+		
+		break;
+		case Complete:
+			
+			
+		break;
+		default: // state is corrupt.
+			
+		
+		break;
+	}
+	return ret_val;	
+}
 /***************WRITE_ENC28_CTRL*********************************************
 *This funciton may be used to write any registers on the ENC28J60			*
 *****************************************************************************/
