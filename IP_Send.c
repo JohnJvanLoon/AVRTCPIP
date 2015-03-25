43757b85ec8409ba36660ee219ce0f31c5ac2201
MystreCoder <MystreCoder@users.noreply.github.com>
2015-03-24 21:21:15 -0400

    Update IP_Send.c
    Added IP_release and IP start from pull request not able to merge.

--- IP_Send.c
+++ IP_Send.c
@@ -6,6 +6,7 @@
  */ 
 
 #include <avr/io.h>
+#include "ETH_send.h"
 #include "IP_Send.h"
 
 typedef enum  {Idle, Attach, Write, S2, S3, S4, S5, S6, Write_Dest_IP, S8, Eth_Send_State, S10, IP_Release} IP_send_comm_states;
@@ -16,6 +17,7 @@ typedef struct
 }IP_send_comm_struct;
 
 volatile IP_send_comm_struct IP_send_comm_data; // global variable for the IP send communication data 
+uint8_t ETH_Send_Release(void);
 
 uint8_t IP_send_comm(void)
 {
@@ -80,4 +82,32 @@ uint8_t IP_send_attach(void)
 	}
 	}
 	return ret_val;
+}
+
+uint8_t IP_Send_Start (void)
+{
+	uint8_t ret_val=0;
+	if (spi_data.len==0 && IP_data.state==S0A)
+	{
+		IP_data.state=S1;
+		ret_val=1;
+	}
+	return ret_val;
+}
+
+uint8_t IP_Send_Release (void)
+{
+	uint8_t ret_val=0;
+	
+	if (ETH_Send_Release(void))
+	{
+		if (spi_data.len==0 && IP_data.state==Complete)
+		{
+			IP_data.state=Idle;
+			ret_val=1;
+		}
+	}
+
+	return ret_val;
+
 }
