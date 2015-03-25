263a5b2fdf64a01a20a8c180742ab738c50b9f63
JohnJvanLoon <john.vanloon@mohawkcollege.ca>
2015-03-24 22:18:00 -0400

    Fixed syntax issues.
    Code now compiles

--- enc28J60.h
+++ enc28J60.h
@@ -220,6 +220,9 @@
 #define SYS_RESET_CMD  0xFF
 
 /// ******************* Definitions *********************
+/// Definitions for the ENC28J60 control Lines
+#define ENC28J60_PORT	PORTB
+#define ENC28J60_CS		PB4
 
 /************************************************************************/
 /* Function declarations                                                */
@@ -227,8 +230,11 @@
 void ENC28J60_init(uint16_t RXsize, uint16_t TXsize, uint8_t Broadcast);
 void ENC28J60_config_LEDs(uint8_t ledA, uint8_t ledB, uint8_t led_stretch);
 void enc28J60_SPI_Init(void);
-int ENC28J60_coms_release(void);
+uint8_t ENC28J60_coms_release(void);
+uint8_t ENC28J60_coms_attach(void);
 void WRITE_ENC28J60_CTRL(uint8_t REGISTER, uint8_t data);
 void BITSET_ENC28J60_CTRL(uint8_t REGISTER, uint8_t data); 
 void BITCLR_ENC28J60_CTRL(uint8_t REGISTER, uint8_t data); 
+
+
 #endif
