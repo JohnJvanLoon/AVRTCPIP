4d006d7c9ecdac3c68fc912eba049bfeab01e026
James Guther <jamison_guther@hotmail.com>
2015-03-31 15:32:30 -0400

    Update IP_Receive.h

--- IP_Receive.h
+++ IP_Receive.h
@@ -8,7 +8,7 @@
 
 #ifndef IP_RECEIVE_H_
 #define IP_RECEIVE_H_
-uint8_t ip_receive_release (void)
-uint8_t ip_receive_request_attach(void)
+uint8_t ip_receive_release (void);
+uint8_t ip_receive_request_attach(void);
 
 #endif /* IP_RECEIVE_H_ */
