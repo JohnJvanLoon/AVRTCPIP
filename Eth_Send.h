b98b0e7bcd46700d9e1fd2876333cd2e018ffbf5
Royburnison <dj_rcb@hotmail.com>
2015-03-31 12:40:03 -0400

    Update Eth_Send.h

--- Eth_Send.h
+++ Eth_Send.h
@@ -6,5 +6,5 @@ uint8_t ETH_Send_Release(void);
 uint8_t ETH_Send_Attach(void);
 uint8_t ETH_Send_Complete(void);
 uint8_t ETH_Send_Comm_Complete(void);
-uint8_t ETH_Send_WR_REG(void);
+uint8_t ETH_Send_Write_REG(void);
 #endif
