bf6b0ab8ac310d212199f043e0062f71b123d4ee
JohnJvanLoon <john.vanloon@mohawkcollege.ca>
2015-04-07 16:20:25 -0400

    Removal of Eth.c and .h from the project

--- IP_Receive.c
+++ IP_Receive.c
@@ -9,7 +9,7 @@
 #include "Eth_Receive.h"
 #include "IP_Receive.h"
 #include "IP_Send.h"
-#include "eth.h"
+#include "Ethernet.h"
 
 typedef enum  {Idle, Attached, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17, Complete} ip_receive_states_t;
 
